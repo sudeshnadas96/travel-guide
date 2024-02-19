54b8f328cf0e7832ffa5a0ad4b12bcef3d283911
unknown <dassudeshna96@gmail.com>
2024-02-20 00:41:53 +0530

    adding functionality to the header

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ window.addEventListener('scroll', () => {
 });
 
 
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw')
+});
+//hide sidebar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != 'navbar-collapse' && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw')
+    }
+});
